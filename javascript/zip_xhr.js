6441ac9097370398f3c3df21081e19a4e11ec9a9
seppzzz <till.moser@ufg.ac.at>
2023-09-26 08:34:43 +0200

    Update zip_xhr.js

--- javascript/zip_xhr.js
+++ javascript/zip_xhr.js
@@ -1,4 +1,46 @@
 
+
+document.addEventListener("DOMContentLoaded", function () {
+    var buttons = document.querySelectorAll(".zipDownloadButton");
+
+    buttons.forEach(function (button) {
+        button.addEventListener("click", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            var href = this.getAttribute('href');
+
+            
+            // Get the base URL from the SilverStripe template variable
+			//var baseHref = '$BaseHref'; // Ensure this is properly rendered in your template
+
+            // Construct the URL for the AJAX request
+            var downloadUrl =  href;
+
+            // Create a new XMLHttpRequest
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', downloadUrl, true);
+            xhr.responseType = 'blob'; // Set responseType to 'blob' to handle binary data
+
+            xhr.onload = function () {
+                if (xhr.status === 200) {
+                    var blob = xhr.response;
+                    var zipName = xhr.getResponseHeader('X-Zip-Name') || 'archive.zip'; // Default name if not provided
+
+                    saveAs(blob, zipName);
+                } else {
+                    // Handle errors here
+                    console.log('Error triggering download');
+                }
+            };
+
+            // Send the AJAX request
+            xhr.send();
+        });
+    });
+});
+
+/*
 	$(document).ready(function () {
 
 		
@@ -12,10 +54,10 @@
 			//console.log(href);
 			
 			// Get the base URL from the SilverStripe template variable
-			// var baseHref = '$BaseHref'; 
+			var baseHref = '$BaseHref'; // Ensure this is properly rendered in your template
 
-			// Construct the URL for the XMLHttpRequest request
-			var downloadUrl = href;
+			// Construct the URL for the AJAX request
+			var downloadUrl = href; // + slug; baseHref + '/' + 
 
 			// Create a new XMLHttpRequest
 			var xhr = new XMLHttpRequest();
@@ -42,3 +84,5 @@
 		});
 		
 	});
+	
+	*/
